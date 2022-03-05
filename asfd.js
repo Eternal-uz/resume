a5e07a582e0571a73425255456fcbd75bb8dc2f5
eternal <eternaltalaba@gmail.com>
2022-03-05 12:46:49 +0500

    first commit

---
+++ asfd.js
@@ -0,0 +1,222 @@
+const resume = {
+    firstName: 'Azizbek',
+    lastName: 'Umaraliev',
+    jobTitle: 'Content Manager who codes',
+    city: 'Tashkent',
+    country: 'Uzbekistan',
+    phone: '+998 93 245 6556',
+    email: 'azizjackbek@gmail.com',
+    education: [{
+        school: 'MDIST',
+        degree: 'Bachelor',
+        graduationDate: '2021',
+        description: 'Business Management'
+    }],
+    education2: [{
+        school2: 'Riga Technical University',
+        degree2: 'Bachelor',
+        graduationDate2: '2015',
+        description2: 'Enterpreneurship and Management'
+    }],
+    links: [{
+            label: 'GitHub',
+            link: 'https://github.com/Eternal-uz'
+        },
+        {
+            label: 'LinkedIn',
+            link: 'https://www.linkedin.com/in/azizbekumaraliev/'
+        },
+        {
+            label: 'Website',
+            link: 'https://my-site-eternal-uz.vercel.app/'
+        },
+        {
+            label: 'Side projects',
+            link: 'https://n-one-ashy.vercel.app/'
+        }
+    ],
+    skills: [
+        'JavaScript',
+        'TypeScript',
+        'Firebase',
+        'Vue js',
+    ],
+    languages: ['English', 'Russian', 'Uzbek', 'French', 'Turkish'],
+    professionalSummary: `Professional with a vast background in service, sales, marketing, management, and with a great personality`,
+    
+    employmentHistory: [{
+            jobTitle: 'Content Manager/Seo Expert',
+            startDate: 'Jan 2020',
+            endDate: 'Present',
+            employer: 'Aurora',
+            city: 'Tashkent',
+            achievements: [
+                'I need to analyze the market and find keywords to write the articles about, and when they are ready I post them to our websites',
+                'It is a management position with a bit of everything. due to the size of the company, I have to help write the articles, and help with everything I can as well'
+            ]
+        },
+        {
+            jobTitle: 'Product manager',
+            startDate: 'Nov 2019',
+            endDate: 'Dec 2019',
+            employer: 'Artel',
+            city: 'Tashkent',
+            achievements: [
+                'I joined the team as the product manager and worked there only for a short period, because it was too much of a corporate environment',
+                'My duties were making reports and takign stock of every type of product we have, and keeping in touch with the sales with updated numbers.',
+                'Basicall, I was responsible for one type of product and had to keep track of what is happening at all times, so that I could order more from the factory',
+            ]
+        },
+        {
+            jobTitle: 'Manager of Language School',
+            startDate: 'Sep 2019',
+            endDate: 'Now 2019',
+            employer: 'Elite School',
+            city: 'Tashkent',
+            achievements: [
+                'I helped manage the language school and did everythign that is necessary for it to grow and helped with everythign else I can as well.',
+            ]
+        },
+        {
+            jobTitle: 'Owner of a cafe',
+            startDate: 'Mar 2019',
+            endDate: 'Sep 2019',
+            employer: 'Self',
+            city: 'Andijan',
+            achievements: [
+                'I have tried to open a small cafe, and it did go well, but that took too much effort and it was almost like I was working for less salary because I had so much expenses.',
+                'I stopped because my parents keep pushing me to finish my degree, so I decided to go to MDIST to finish my degree',
+            ]
+        },
+        {
+            jobTitle: 'Chief Conciege',
+            startDate: 'MM 2018',
+            endDate: 'Mar 2019',
+            employer: 'Mondrain Doha',
+            city: 'Doha',
+            achievements: [
+                'I was responsible for a team of bellmen, and concierge. I had to make their schedule, make sure they don\'t fight, and print reports and enter them into micros',
+                'I left because I had missed my country and I was away for far too long. plus I also had some money saved up'
+            ]
+        },
+        {
+            jobTitle: 'Assistance Manager',
+            startDate: 'MM 2018',
+            endDate: 'MM 2018',
+            employer: 'Dubai mall',
+            city: 'Dubai',
+            achievements: [
+                `I worked for a short period as the asistane manager of a shop that sold shoes, formal shoes, I left because I was tired of Dubai.`
+            ]
+        },
+        {
+            jobTitle: 'Head Waiter',
+            startDate: 'MM 2018',
+            endDate: 'MM 2018',
+            employer: 'Marriot',
+            city: 'Dubai',
+            achievements: [
+                'This is a place where I had worked before and loved it I had first worked as a waiter.',
+                'Defined and implemented a Code-First approach, using ORMs such as Entity Framework to manipulate the database from code.'
+            ]
+        },
+        {
+            jobTitle: 'Sales Associate ',
+            startDate: 'MM 2017',
+            endDate: 'MM 2017',
+            employer: 'Dubai Mall',
+            city: 'Dubai',
+            achievements: [
+                'After having worked in various fields, I decided that I have to try a sales position. I always thought I was good at sales, here I learned that I was not as good at I thought, but good enough to be promoted.',
+                'I left it because I did not know what I wanted to do, and I was living with a few guys that I did not get along, and I couldn\'t afford a place on my own, so I decided to go back to my hotel.'
+            ]
+        },
+        {
+            jobTitle: 'Waiter',
+            startDate: 'MM 2017',
+            endDate: 'Aug 2016',
+            employer: 'Jumierah Al Naseem',
+            city: 'Dubai',
+            achievements: [
+                'Firt time in Dubai, I was part of a pre opening team at Jumierah al nassem hotel, it was overall a good experince that taught me to be modest, espeically if you are son of a normal person )).',
+            ]
+        },
+        {
+            jobTitle: 'Chef',
+            startDate: 'Sep 2012',
+            endDate: 'Mar 2014',
+            employer: 'Various Employers',
+            city: 'Riga',
+            achievements: [
+                'I worked in varius different restaurants as chef for quite a while. I have worked on and off, I just don\'t remember the name of each restaurant',
+            ]
+        },
+        {
+            jobTitle: 'Chef',
+            startDate: 'May 2014',
+            endDate: 'Sep 2014',
+            employer: 'Best Western Plus',
+            city: 'Freiburg',
+            achievements: [
+                'I was a student in Riga, which allowed me to travel across europe easily. so, I found this job through an agency that sent sudents as a sort of work and travel kind of program to work in the remote areas of the Germany',
+                'I worked here as part of the coking crew, I mainly focuses on learning the langauge and having fun but I also learnt how to make a great germany beer just out of yeast and a juice box'
+            ]
+        },
+        {
+            jobTitle: 'Chef',
+            startDate: 'May 2013',
+            endDate: 'Sep 2013',
+            employer: 'turkish Kebab',
+            city: 'Oslo',
+            achievements: [
+                'Having worked as a chef for a  few years, and living with many turkish people. I got accuainted with a guy who invited me to work in Oslo during the summer, which I did.',
+                'However, at the end, because Norway was not part of the proper EU, I had no legal rights to work he did not pay me for the last month and a half (I still remember because it was a lot of money for) instead called the police on me who then sent me back to Riga'
+            ]
+        },
+        {
+            jobTitle: 'Chef',
+            startDate: 'Sep 2011',
+            endDate: 'MM 2012',
+            employer: 'International Travel Network',
+            city: 'San Fransisco',
+            achievements: [
+                'I worked as travel agent from Riga. I was like a call center but helped me with gaining a lot professional experince.',
+            ]
+        },
+        {
+            jobTitle: 'Reception',
+            startDate: 'MM 2009',
+            endDate: 'MM 2010',
+            employer: 'Elita Hotel',
+            city: 'Andijan',
+            achievements: [
+                'One of the first jobs I have ever done was working as a reception in one of the hotels in Andijan, it was a good experience.',
+            ]
+        }
+    ],
+    // photo: 'https://postimg.cc/f37sp80h',
+}
+
+const formatResume = (r) => ({
+    ...r,
+    address: [
+        r.country,
+        r.city,
+        r.postalCode
+    ].filter(Boolean).join(', ')
+})
+
+new Vue({
+    el: "#app",
+    data: formatResume(resume)
+});
+
+/**
+ * Wait for animatable-component to be loaded (Only for VanillaJS)
+ **/
+function animatableLoaded() {
+    document.querySelector('body').classList.remove('d-none');
+}
+if (customElements) {
+    customElements.whenDefined('animatable-component').then(animatableLoaded);
+} else animatableLoaded()
